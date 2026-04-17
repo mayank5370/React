4582fad7d990dcdeca7917a3670cdd0e1857a303
mayank5370 <mayank.1290singh@gmail.com>
2026-04-18 03:05:03 +0530

    Cahnge in Checkout Page

--- ecimmerce-project/src/App.jsx
+++ ecimmerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
+import {CheckoutPage} from './pages/CheckoutPage';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path ="checkout" />   
+      <Route path ="checkout" element={<CheckoutPage/>} />   
     </Routes>
 
   );
