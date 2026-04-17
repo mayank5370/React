791d587d5ff1d4ac5805cd3cc45e85a21335a22c
mayank5370 <mayank.1290singh@gmail.com>
2026-04-18 03:19:34 +0530

    Add Order Page

--- ecimmerce-project/src/App.jsx
+++ ecimmerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import {CheckoutPage} from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path ="checkout" element={<CheckoutPage/>} />   
+      <Route path ="checkout" element={<CheckoutPage/>} />  
+      <Route path = "orders" element= {<OrdersPage/>} /> 
     </Routes>
 
   );
