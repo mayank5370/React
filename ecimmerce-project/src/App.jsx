eb227c40a9cdf2d151337887dcaeff19aea23452
mayank5370 <mayank.1290singh@gmail.com>
2026-04-18 02:49:24 +0530

    create Route

--- ecimmerce-project/src/App.jsx
+++ ecimmerce-project/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
-    <HomePage />
-    
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path ="checkout" />   
+    </Routes>
+
   );
 }
 
